80740aa23a54f262ab6090722446be852b91f542
Bryan Mytko <bryanmytko@gmail.com>
2016-06-05 04:24:21 -0400

    Move adventures text to its own file

---
+++ scripts/adventures.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    "id": 0,
+    "image": "http://i.imgur.com/SfL5rj2.jpg",
+    "text": "You awake lying in the bracken of a dark, twisted forest. Your back and head are killing you. Reaching up to touch your temple, you wince with pain and your fingertips come away bloody. \nYou see faint, flickering light to the East of you. There's a frenzied stomping and snorting coming from the North. To the West there's nothing but more black, twisted forest. Suddenly, you hear rustling coming from the underbrush to the South.",
+    "options": [
+      { "label": "Go North", "to": 1 },
+      { "label": "Go East", "to": 1 },
+      { "label": "Go South", "to": 1 },
+      { "label": "Go West", "to": 1 }
+    ]
+  },
+
+  {
+    "id": 1,
+    "image": "http://i.imgur.com/Qgbz7QF.jpg",
+    "text": "You approach a bridge in the gloomy woods. As the wind blows through the trees, you can hear the boards creak as the bridge gently sways. It's impossible to see what is below or how far across the bridge stretches...",
+    "options": [
+      { "label": "Try to cross the bridge", "to": 2 },
+      { "label": "Turn around", "to": 0 },
+    ]
+  },
+
+  {
+    "id": 1,
+    "image": "https://intimacywithjesus.files.wordpress.com/2014/06/drowning-girl-sea-water-favim-com-112419.jpg",
+    "text": "As you step across the bridge you hear the ropes begin to snap. You try to dash to the other side but the bridge twists and the planks detach from under your feet. As you plummet into the cold, dark water below you hear a faint echo of laughter from the woods above.",
+  },
+
+];
