0f8e958a093624ce85f681d6a117bbc5c6bde902
Bryan Mytko <bryanmytko@gmail.com>
2016-06-07 15:57:36 -0400

    Change default port to 9292

--- server.js
+++ server.js
@@ -1,28 +1,22 @@
 var express = require('express'),
   path = require('path'),
   app = express(),
-  port = 4444,
+  port = 9292,
   bodyParser = require('body-parser');
 
-// Make sure to include the JSX transpiler
-// require('node-jsx').install();
-//
-// // Include static assets. Not advised for production
-// app.use(express.static(path.join(__dirname, 'public')));
-// // Set view path
-// app.set('views', path.join(__dirname, 'views'));
-// // set up ejs for templating. You can use whatever
-// app.set('view engine', 'ejs');
-//
-// // Set up Routes for the application
-// require('./app/routes/core-routes.js')(app);
-//
-// //Route not found -- Set 404
-// app.get('*', function(req, res) {
-//     res.json({
-//             'route': 'Sorry this page does not exist!'
-//                 });
-//                 });
-//
-//                 app.listen(port);
-//                 console.log('Server is Up and Running at Port : ' + port);
+require('node-jsx').install();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+require('./app/routes/core-routes.js')(app);
+
+app.get('*', function(req, res) {
+  res.json({
+    'route': '404 - Page Not Found'
+  });
+});
+
+app.listen(port);
+console.log('Server running on port: ' + port);
