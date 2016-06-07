5146abb07581708b349baa874495f203e05bea2d
Bryan Mytko <bryanmytko@gmail.com>
2016-06-07 16:13:31 -0400

    Fix routes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-require('./app/routes/core-routes.js')(app);
+require('./app/routes')(app);
 
 app.get('*', function(req, res) {
   res.json({
