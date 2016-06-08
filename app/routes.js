a8609315a4b420926b7a84c456b9a4144eb877ab
Bryan Mytko <bryanmytko@gmail.com>
2016-06-08 14:56:25 -0400

    Remove extra routing stuff

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,5 @@
-var React = require('react'),
-    ReactApp = React.createFactory(require('../app/main'));
-
 module.exports = function(app) {
   app.get('/', function(req, res){
-    var reactHtml = React.renderToString(ReactApp({}));
-    res.render('index.ejs', { reactOutput: reactHtml });
+    res.render('index.ejs');
   });
 };
